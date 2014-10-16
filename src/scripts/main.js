51c4b4227b7c5a52d2a0f9d976c83ec0f124cdf8
Alex Jordan <alexander3223098@gmail.com>
2014-10-15 17:50:55 -0700

    Tabify

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,24 @@
 // Require Node modules in the browser thanks to Browserify: http://browserify.org
 var bespoke = require('bespoke'),
-  cube = require('bespoke-theme-cube'),
-  keys = require('bespoke-keys'),
-  touch = require('bespoke-touch'),
-  bullets = require('bespoke-bullets'),
-  backdrop = require('bespoke-backdrop'),
-  scale = require('bespoke-scale'),
-  hash = require('bespoke-hash'),
-  progress = require('bespoke-progress');
+	cube = require('bespoke-theme-cube'),
+	keys = require('bespoke-keys'),
+	touch = require('bespoke-touch'),
+	bullets = require('bespoke-bullets'),
+	backdrop = require('bespoke-backdrop'),
+	scale = require('bespoke-scale'),
+	hash = require('bespoke-hash'),
+	progress = require('bespoke-progress');
 
 // Bespoke.js
 bespoke.from('article', [
-  cube(),
-  keys(),
-  touch(),
-  bullets('li, .bullet'),
-  backdrop(),
-  scale(),
-  hash(),
-  progress()
+	cube(),
+	keys(),
+	touch(),
+	bullets('li, .bullet'),
+	backdrop(),
+	scale(),
+	hash(),
+	progress()
 ]);
 
 // Prism syntax highlighting
